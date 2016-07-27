31964b889e9ce6a10087c0e473b60469a933fcb5
Sam Carroll <esamuel1@gmail.com>
2016-07-26 21:45:52 -0500

    Building object structure for database.

--- src/ClassTracker.Core/Objects/Parents.cs
+++ src/ClassTracker.Core/Objects/Parents.cs
@@ -5,15 +5,17 @@ using System.Threading.Tasks;
 
 namespace ClassTracker.Core.Objects
 {
-    public class Authors
+    public class Parents
     {
-        public Authors() {
+        public Parents() {
             Id = Guid.NewGuid();
         }
         public Guid Id { get; set; }
-        public String FirstName { get; set; }
+        public String   FirstName   { get; set; }
         public String LastName { get; set; }
+        public String EmailAddress { get; set; }
+        public int ContactNumber { get; set; }
+        public String SecPas { get; set; }
         public DateTime DateAdded { get; set; }
-
     }
 }
--- src/ClassTracker.Core/Objects/Students.cs
+++ src/ClassTracker.Core/Objects/Students.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace 
+namespace ClassTracker.Core.Objects
 {
     public class Students
     {
